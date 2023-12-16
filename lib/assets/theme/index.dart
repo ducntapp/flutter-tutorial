153d1e6bfee94fd7a984c1ea729087f743d9df63
Michael <ducntapp@gmail.com>
2023-12-16 16:46:49 +0700

    init google_font

--- lib/assets/theme/index.dart
+++ lib/assets/theme/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 final dartTheme = ThemeData.dark().copyWith(
   colorScheme: ColorScheme.fromSeed(
@@ -9,3 +10,20 @@ final dartTheme = ThemeData.dark().copyWith(
   scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
 );
 
+final nativeColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 54, 54, 54),
+  brightness: Brightness.dark,
+  background: Color.fromARGB(255, 59, 58, 59),
+);
+
+final nativeTheme = ThemeData().copyWith(
+  colorScheme: nativeColorScheme,
+  textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
+    titleSmall: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.normal),
+    titleMedium: GoogleFonts.ubuntuCondensed(
+      fontWeight: FontWeight.w500,
+    ),
+    titleLarge: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
+  ),
+  scaffoldBackgroundColor: nativeColorScheme.background,
+);
